296cdbd06ab902e0bd015a869a1e94d31d92a7aa
Dave Manning <dmanning@paybyphone.com>
2015-12-21 14:05:54 -0800

    Closes #8 - Response metrics for unhandled exceptions Added InstrumentActionMessageHandler to provide support for instrumenting responses later in the pipeline.

--- StatsDHelper.WebApi/Services/IInstrumentationService.cs
+++ StatsDHelper.WebApi/Services/IInstrumentationService.cs
@@ -1,9 +1,12 @@
-﻿using System.Web.Http.Filters;
+﻿using System.Net.Http;
+using System.Web.Http.Filters;
 
 namespace StatsDHelper.WebApi.Services
 {
     internal interface IInstrumentationService
     {
+        void TimeRequest(HttpRequestMessage request);
         void InstrumentResponse(HttpActionExecutedContext httpActionExecutedContext, string template = "{action}");
+        void InstrumentResponse(HttpResponseMessage response, string template = "{action}");
     }
 }
